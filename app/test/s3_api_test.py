bd7e4c73fe4d3b597a0efddaaf67a4256afc8daf
gcatanese <gcatanese@yahoo.com>
2021-11-11 12:06:51 +0100

    Refactor test

--- app/test/s3_api_test.py
+++ app/test/s3_api_test.py
@@ -8,13 +8,19 @@ class S3ApiTest(unittest.TestCase):
     def test_download(self):
         bucket = 'beppe-udacity'
         key = 'capstone/us-cities-demographics.csv'
-        filename = 'tmp/us-cities-demographics.csv'
+        filename = '../tmp/us-cities-demographics.csv'
 
         download_object(bucket, key, filename)
 
     def test_upload(self):
         bucket = 'beppe-udacity'
-        key = 'capstone/us-cities-demographics-tmp.csv'
-        filename = 'tmp/us-cities-demographics.csv'
+        key = 'doc/S3.md'
+        filename = '../../doc/S3.md'
 
         upload_object(bucket, key, filename)
+
+    def test_bucket_list(self):
+
+        buckets = bucket_list()
+        print(buckets)
+
